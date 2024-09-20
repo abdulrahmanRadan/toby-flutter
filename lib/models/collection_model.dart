a848bf4b63aedc5a00e198f47d8250598e61928c
Hetari <hetari4py@gmail.com>
2024-09-20 21:53:11 +0300

    add tabs and tags in collections

--- lib/models/collection_model.dart
+++ lib/models/collection_model.dart
@@ -1,15 +1,28 @@
+import 'package:toby1/models/tag_model.dart';
+import 'package:toby1/models/tab_model.dart';
+
 class Collection {
   final int id;
   final String title;
   final String? description;
+  final List<AppTab> tabs;
+  final List<Tag> tags;
 
-  Collection({required this.id, required this.title, this.description});
+  Collection({
+    required this.id,
+    required this.title,
+    this.description,
+    required this.tabs,
+    required this.tags,
+  });
 
   factory Collection.fromJson(Map<String, dynamic> json) {
     return Collection(
       id: json['id'],
       title: json['title'],
       description: json['description'],
+      tabs: List<AppTab>.from(json['tabs'].map((tab) => AppTab.fromJson(tab))),
+      tags: List<Tag>.from(json['tags'].map((tag) => Tag.fromJson(tag))),
     );
   }
 }
